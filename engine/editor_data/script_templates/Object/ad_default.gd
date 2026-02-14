fb7d9fc5b3bea42c148a376afd0c3bd83d15cdcf
kubaofc123 <kuba@2222.pl>
2026-02-14 11:40:18 +0100

    init pt2

---
+++ engine/editor_data/script_templates/Object/ad_default.gd
@@ -0,0 +1,37 @@
+# meta-name: Astrum Dominatus Template
+# meta-description: Default Astrum Dominatus Template
+# meta-default: true
+# meta-space-indent: 4
+
+class_name _MyClass
+extends _BASE_
+
+## Part of [Node], main description ends here
+##
+## Long description goes here.
+## [br][br]
+## Uses [method Node._enter_tree]
+## [br][br]
+## @tutorial(Youtube Godot Docs Tutorial): https://youtu.be/B7G6sVNDczY
+## @tutorial(Godot Docs): https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_documentation_comments.html
+
+#=============================== VARIABLES ===============================
+
+#================ PUBLIC ================
+
+#================ PRIVATE ================
+
+#=============================== FUNCTIONS ===============================
+
+#================ PUBLIC ================
+
+#================ PRIVATE ================
+
+# This allows for conditional setting of variables, like hiding from inspector. Requires @tool to work.
+#func _validate_property(property: Dictionary):
+	#if property.name == "resource_local_to_scene":
+		#resource_local_to_scene = true
+
+#=============================== CALLBACKS ===============================
+
+########################## END OF FILE ##########################
